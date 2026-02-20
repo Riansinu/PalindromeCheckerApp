e910aeced77480ddb74d50397e95da82cf0f1d54
Riansinu <riansinu3@gmail.com>
2026-02-20 10:20:20 +0530

    UC3: Implement palindrome check using string reverse logic

--- src/Palindrome/UseCasePalindromeCheckerApp.java
+++ src/Palindrome/UseCasePalindromeCheckerApp.java
@@ -2,11 +2,12 @@ package Palindrome;
 
 public class UseCasePalindromeCheckerApp {
    public  static void main(String[] args) {
+       //use case 1
        System.out.println("Welcome to  palindrome");
        System.out.println("version 1");
        System.out.println("System initialized successfully");
        String word ="madam";
-
+       //use caqse 2
        boolean isPalindrome = true;
 
        for(int i = 0; i < word.length() / 2; i++) {
@@ -21,6 +22,18 @@ public class UseCasePalindromeCheckerApp {
        }
        else {
            System.out.println("Is it palindrome: False");
+       }
+       //use case 3
+       String reversed = "";
+
+       for (int i = word.length() - 1; i >= 0; i--) {
+           reversed = reversed + word.charAt(i);
+       }
+
+       if (word.equals(reversed)) {
+           System.out.println("UC3 Result (String Reverse): True");
+       } else {
+           System.out.println("UC3 Result (String Reverse): False");
        }
 
    }
