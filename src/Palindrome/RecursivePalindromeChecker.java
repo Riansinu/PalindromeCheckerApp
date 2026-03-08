6afb5bbf3c94cfe1d379eb1e380fc204381ac9bf
Riansinu <riansinu3@gmail.com>
2026-03-09 00:02:12 +0530

    UC9: Implement palindrome check using recursion

---
+++ src/Palindrome/RecursivePalindromeChecker.java
@@ -0,0 +1,32 @@
+package Palindrome;
+
+public class RecursivePalindromeChecker {
+
+    // Recursive method
+    public static boolean isPalindrome(String str, int start, int end) {
+
+        // Base condition
+        if (start >= end) {
+            return true;
+        }
+
+        // If characters do not match
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        String str = "racecar";
+
+        if (isPalindrome(str, 0, str.length() - 1)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
