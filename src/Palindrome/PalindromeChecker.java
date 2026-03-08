f0caf2f9869c081717d539abd400b2d1beabf5d9
Riansinu <riansinu3@gmail.com>
2026-03-08 21:08:14 +0530

    UC8: Implement palindrome check using singly linked list

---
+++ src/Palindrome/PalindromeChecker.java
@@ -0,0 +1,96 @@
+package Palindrome;
+
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class PalindromeChecker {
+
+    Node head;
+
+    // Add node to linked list
+    public void add(char data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+    }
+
+    // Reverse linked list
+    private Node reverse(Node node) {
+        Node prev = null;
+        Node current = node;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+
+    // Check palindrome
+    public boolean isPalindrome() {
+
+        if (head == null || head.next == null)
+            return true;
+
+        Node slow = head;
+        Node fast = head;
+
+        // Find middle
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        Node secondHalf = reverse(slow.next);
+
+        Node firstHalf = head;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data)
+                return false;
+
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String str = "madam";
+
+        PalindromeChecker list = new PalindromeChecker();
+
+        for (char c : str.toCharArray()) {
+            list.add(c);
+        }
+
+        if (list.isPalindrome())
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
--- src/Palindrome/UseCasePalindromeCheckerApp.java
+++
@@ -1,7 +0,0 @@
-package Palindrome;
-
-public class UseCasePalindromeCheckerApp {
-   public  static void main(String[] args) {
-       System.out.println("Welcome palindrome");
-    }
-}
